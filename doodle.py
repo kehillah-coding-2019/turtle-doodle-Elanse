162dda6083d79942eb3c3d14b542137b388bf686
Elanse <33039002+Elanse@users.noreply.github.com>
2018-10-04 14:32:09 -0700

    set window to correct aspectRatio

--- doodle.py
+++ doodle.py
@@ -3,22 +3,30 @@ import turtle
 bg = turtle.Screen()
 cross = turtle.Turtle()
 
-bg.screensize(1080, 540, "midnightblue") #sets flag background
+height = 400
+aspectRatio = 2/1
+length = aspectRatio * height
+
+bg.setup(length, height) #sets flag dimensions in a 2:1 aspectRatio
+bg.bgcolor("midnightblue")
+
 
 """
-Make a function to draw the diagonal lines on the Union Jack
+Make a function to draw the diagonal lines on the Union Jack.
 """
 
-def crossingLines(myTurtle):
+def diagonalLine(myTurtle, x, y, angle):
     myTurtle.penup()
-    myTurtle.goto(-540, 270)
+    myTurtle.goto(x, y)
     myTurtle.pendown()
-
     myTurtle.pencolor("white")
-    myTurtle.pensize(108)
+    myTurtle.pensize(height/5)
     myTurtle.begin_fill()
-    myTurtle.right(45)
-    myTurtle.forward(1207.5)
+    myTurtle.right(angle)
+    myTurtle.forward(675)
     myTurtle.end_fill()
+    myTurtle.ht()
+
+diagonalLine(cross, -300, 150, 45)
 
 input()
