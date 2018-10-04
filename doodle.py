a2d3e41df75d1776e9dc001b9a8efb6deeae98b9
Elanse <33039002+Elanse@users.noreply.github.com>
2018-10-04 13:52:00 -0700

    create a function to create diagonal lines on the Union Jack

--- doodle.py
+++ doodle.py
@@ -1,8 +1,24 @@
 import turtle
 
 bg = turtle.Screen()
+cross = turtle.Turtle()
 
 bg.screensize(1080, 540, "midnightblue") #sets flag background
 
+"""
+Make a function to draw the diagonal lines on the Union Jack
+"""
+
+def crossingLines(myTurtle):
+    myTurtle.penup()
+    myTurtle.goto(-540, 270)
+    myTurtle.pendown()
+
+    myTurtle.pencolor("white")
+    myTurtle.pensize(108)
+    myTurtle.begin_fill()
+    myTurtle.right(45)
+    myTurtle.forward(1207.5)
+    myTurtle.end_fill()
 
 input()
