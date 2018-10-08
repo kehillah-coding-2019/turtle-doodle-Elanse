e6498ae10e16f55fe7ced7bcfccb0c7905278cf2
Elanse <33039002+Elanse@users.noreply.github.com>
2018-10-08 15:30:26 -0700

    complete flag

--- doodle.py
+++ doodle.py
@@ -11,6 +11,11 @@ diagonal1 = turtle.Turtle()
 diagonal2 = turtle.Turtle()
 diagonal3 = turtle.Turtle()
 
+line = turtle.Turtle()
+line1 = turtle.Turtle()
+line2 = turtle.Turtle()
+line3 = turtle.Turtle()
+
 height = 400
 aspectRatio = 2/1 #sets the flag to a 2:1 ratio
 length = aspectRatio * height
@@ -25,7 +30,7 @@ Make a function to draw the diagonal lines on the Union Jack with four parameter
 myTurtle, location on x axis, location on y axis, and angle of the line
 """
 
-def diagonalLine(myTurtle, color, size, x, y, length, angle):
+def drawLine(myTurtle, color, size, x, y, length, angle):
     myTurtle.speed(0)
     myTurtle.penup()
     myTurtle.goto(x, y)
@@ -38,14 +43,15 @@ def diagonalLine(myTurtle, color, size, x, y, length, angle):
     myTurtle.end_fill()
     myTurtle.ht()
 
-diagonalLine(cross, "white", height/5, -400, 200, 900, 26.7) # first white cross
-diagonalLine(cross2, "white", height/5, 400, 200, 900, 153.3) # second white cross
-diagonalLine(diagonal, "red", height/15, -413.3, 200, 425, 26.7) # top left
-diagonalLine(diagonal1, "red", height/15, 386.7, 200, 425, 153.3) # top right
-diagonalLine(diagonal2, "red", height/15, -386.7, -200, 425, -26.7) # bottom left
-diagonalLine(diagonal3, "red", height/15, 413.3, -200, 425, -153.3)
-
-
-
+drawLine(cross, "white", height/5, -400, 200, 900, 26.7) # first white cross
+drawLine(cross2, "white", height/5, 400, 200, 900, 153.3) # second white cross
+drawLine(diagonal, "red", height/15, -413.3, 200, 425, 26.7) # top left
+drawLine(diagonal1, "red", height/15, 386.7, 200, 425, 153.3) # top right
+drawLine(diagonal2, "red", height/15, -386.7, -200, 425, -26.7) # bottom left
+drawLine(diagonal3, "red", height/15, 413.3, -200, 425, -153.3) # bottom right
+drawLine(line, "white", height/3, 0, 200, 400, 90)
+drawLine(line1, "white", height/3, -400, 0, 800, 0)
+drawLine(line2, "red", height/5, 0, 200, 400, 90)
+drawLine(line3, "red", height/5, -400, 0, 800, 0)
 
 input()
